60d25f6b4f2b5cccde02329f3dc26976124e5a84
GhassanYusuf <ghassan.yousif.83@gmail.com>
2022-10-28 14:10:40 +0300

    the first upload

---
+++ IRSensor.h
@@ -0,0 +1,56 @@
+
+  class IRSensor {
+
+    private:
+
+      // Pin Settings
+      uint8_t   _pin        = 0;
+      bool      _invert     = false;
+      int       _threshold  = 50;
+
+    public:
+
+      // Constructor
+      IRSensor(uint8_t input) {
+        _pin = input;
+      }
+
+      // Inverts The Reading Result
+      void Invert() {
+        _invert = !_invert;
+      }
+
+      // Setting The Threshold
+      void Threshold(int input) {
+        _threshold = input;
+      }
+
+      // Function For Decision Making
+      bool Read() {
+
+        // Reading The Input And Maping It
+        int reading = map(analogRead(_pin), 0, 1023, 0, 100);
+
+        // Checking For Invert Signal
+        if(_invert == false) {
+
+          // Return Not Inverted Decision
+          if(reading > _threshold) {
+            return true;
+          } else {
+            return false;
+          }
+          
+        } else {
+          
+          // Return Inverted Decision
+          if(reading > _threshold) {
+            return false;
+          } else {
+            return true;
+          }
+        }
+        
+      }
+    
+  };
